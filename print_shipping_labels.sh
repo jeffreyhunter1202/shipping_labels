d40e07112adafdffd63fd98e685d550cff4ce8b3
jeffreyhunter1202 <90942922+jeffreyhunter1202@users.noreply.github.com>
2021-09-17 20:27:34 -0500

    Update print_shipping_labels.sh

--- print_shipping_labels.sh
+++ print_shipping_labels.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+# Installed at /cron/print_shipping_labels.sh
+# Created: September 2021
 
 print_labels() {
   echo "[+] Printing shipping labels"
 }
+
+# Check we're running as root
+if [[ $EUID -ne 0 ]]; then
+   echo "[!] This script must be run as root" 
+   exit 1
+fi
+
+# Check that we have an active internet connection
+wget -q --spider http://google.com
+if [ $? -ne 0 ]; then
+    echo "[!] Please ensure this machine has internet access."
+    exit 1
+fi
 
 echo "[+] Starting $(date)"
 
