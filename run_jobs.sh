27fec5d986591cddd493f89b5a74087bd4266c54
jeffreyhunter1202 <90942922+jeffreyhunter1202@users.noreply.github.com>
2021-09-24 02:10:40 -0500

    Update run_jobs.sh

--- run_jobs.sh
+++ run_jobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Run all my jobs!
-for file in /cron/*.job.sh
+for file in /cron/jobs/*.job.sh
 do
   echo "[+] Running job=($file) at $(date)"
   # Be sure not to wait for any one job to complete; fire and forget
